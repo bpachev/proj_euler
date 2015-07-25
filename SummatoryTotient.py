9a514e5f0a88e208f3a9e970ba74422e8a21d0ce
bpachev <benjaminpachev@gmail.com>
2015-07-24 18:51:33 -0600

    Fixed bugs in the totient summatory class and added lambda to Titanic.py

--- SummatoryTotient.py
+++ SummatoryTotient.py
@@ -1,3 +1,4 @@
+import numpy as np
 class TotientSum:
  '''
  A class to find sums involving Euler's totient function.
@@ -7,12 +8,12 @@ class TotientSum:
  of the form g(cap/i) in the sum. The expression cap/i takes on O(sqrt(cap)) distinct values, so the weighted sum may be computed from particular cached values of partial sums of the non-weighted sum. The weighted sum may be computed via the definition for i <= sqrt(n). Let F(n) denote the non-weighted sum up to n. Then the appropriate multiplier by G(k), k > sqrt(n), is F(cap/k) - F(cap/(k-1)). These partial sums are cached as part of the computation of F(cap).
  '''
  def __init__(self,cap,f,fSum,g,mod=10**9,totcache=None):
- '''
- fSum(i,j) is equivalent to sum([f(i) for i in xrange(i,j)])
- It had better not be linear time though.
- g(n) = sum i=1 to n of i*f(i)
- Should also be constant time.
- '''
+   '''
+   fSum(i,j) is equivalent to sum([f(i) for i in xrange(i,j)])
+   It had better not be linear time though.
+   g(n) = sum i=1 to n of i*f(i)
+   Should also be constant time.
+   '''
    self.maxK = int(cap**.5)
    self.kCache = np.zeros(self.maxK+1,dtype=np.int64) #partial sum up to cap/k
    self.mod = mod
@@ -36,22 +37,22 @@ class TotientSum:
       if totSum[i] == i:
         for j in xrange(i,self.cache_size,i):
           totSum[j] = totSum[j] - totSum[j]/i
-      totSum[i] = (f(i)*totSum[i] + totSum[i-1]) % mod
+      totSum[i] = ((f(i)%mod)*totSum[i] + totSum[i-1]) % self.mod
      self.sCache = totSum
  
- def Sum(self,n,k):
+ def Sum(self,n,k=1):
    if n < self.cache_size:
      return self.sCache[n]
    if k < self.maxK and self.kCache[k]:
      return self.kCache[k]
    
-   s = g(n,self.mod)
+   s = self.g(n,self.mod)
    
    q,div = n/2,2
    while q >= 1:
     r = n % div
     div_next = div + r/q + 1
-    s = (s - self.fSum(div,div_next) * self.Sum(n/div,k*div) ) % mod
+    s = (s - self.fSum(div,div_next,self.mod) * self.Sum(n/div,k*div) ) % self.mod
     div = div_next
     q = n/div_next
    if k < self.maxK:
@@ -65,10 +66,11 @@ class TotientSum:
   
   s = 0
   for i in xrange(start,self.maxK+1):
-    s = ( s + w(self.cap/i) * (F(i)-F(i-1))) % self.mod
+    s = ( s + (w(self.cap/i)%self.mod) * (self.Sum(i)-self.Sum(i-1))) % self.mod
+    print s,i
   
   for k in xrange(1,self.maxK):
-    s = (s + w(k) * (F(n/k) - F(n/(k+1))) % self.mod
+    s = (s + (w(k)%self.mod) * (self.Sum(self.cap/k,k) - self.Sum(self.cap/(k+1),k+1))) % self.mod
   return s
    
   
--- Titanic.py
+++ Titanic.py
@@ -1,9 +1,33 @@
 from fractions import gcd
+from SummatoryTotient import TotientSum
+
+
 
 #This works for n = 111
 n=111
 s = 0
 mod = 10**8
+
+#functions to sum tot(n)
+f0 = lambda x: 1
+g0 = lambda n,m: (n*(n+1)/2)%m
+fsum0 = lambda x,y,m: (y-x)%m
+
+#functions to sum n*tot(n)
+f1 = lambda x: x
+g1 = lambda n,m: (n*(n+1)*(2*n+1)/6)%m
+fsum1 = lambda x,y,m: g0(y-1,m)-g0(x-1,m)
+
+#functions to sum n^2*tot(n)
+f2 = lambda x: x*x
+g2 = lambda n,m: ((n*(n+1)/2)**2)%m
+fsum2 = lambda x,y,m: g1(y-1,m)-g1(x-1,m)
+
+t0 = TotientSum(6,f0,fsum0,g0,mod)
+print t0.weighted_sum(lambda x:1)
+t2 = TotientSum(6,f1,fsum1,g1,mod)
+t2 = TotientSum(6,f2,fsum2,g2,mod)
+
 def f(x):
  global mod
  if x > 2: 
@@ -17,29 +41,37 @@ def f_sum(m):
  return (pow(2,m+1,mod) - 1 - (m+1)*m*(m-1)/6 - ((m+1)*m)/2 - (m+1)) % mod
  
 def T(n,d,k):
+ global mod
  t = 0
 # for l in xrange(3,n/d+1):
   #leftovers from playing around with code, will fix
 #  t += f(l)*d*k
 #  t += d*f(l)*k
- t += 2*d*k*f_sum(n/d)
- t += f(n/d+1)*(n-k*(n/d)+1)*(n%d+1)
- t += f(n/d) * (n-k*(n/d)+1-k)*(d-n%d-1)
+ m = n/d
+ t += 2*d*k*f_sum(m)
+ t += f(m+1)*((n+1)**2 -(k+d)*m*(n+1)+d*k*m*m)
+ t += f(m)*(-(n+1)**2 +(d+k)*(n+1)*(m+1)-d*k*(m+1)*(m+1))
+ 
+# t += f(n/d+1)*(n-k*(m)+1)*(n%d+1)
+# t += f(n/d) * (n-k*(m)+1-k)*(d-n%d-1)
  #if k = d-1 what about negativity?
  #Not sure, but it works
   
 # print t,d,k
  return t%mod
 
-for d in xrange(1,n+1):
-  for k in xrange(1,d+1):
-    if gcd(k,d) == 1:
-      if d>1:
-        s += 4*T(n,d,k)
-      else:
-        s += 2*T(n,d,k)
+def slow_titanic(n):
+ s = 0
+ for d in xrange(1,n+1):
+   for k in xrange(1,d+1):
+     if gcd(k,d) == 1:
+       if d>1:
+         s += 4*T(n,d,k)
+       else:
+         s += 2*T(n,d,k)
+ return s
 
-print (pow(2,(n+1)**2,mod) -1 - s - (n+1)*2*f(n+1) - (n+1)**2)%mod
+print (pow(2,(n+1)**2,mod) -1 - slow_titanic(n) - (n+1)*2*f(n+1) - (n+1)**2)%mod
       
 
 
