cff161035e1e9ec3afe1c857a1b526c573dfffd1
bpachev <benjaminpachev@gmail.com>
2015-10-05 18:08:40 -0600

    Solved 528.

--- e528.py
+++ e528.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.misc import comb
-from proj_euler import safe_matrix_vector_dot,safe_matrix_mul,safe_matrix_exp,matrix_mod_exp
+from proj_euler import safe_matrix_mul
 from itertools import izip
 
 def s(n,k,b,mod = 10**9+7):
@@ -16,6 +16,7 @@ def s(n,k,b,mod = 10**9+7):
   cache.append(accum)
   accum = safe_matrix_mul(accum,accum,mod)
   tn/=2
+ cache.append(accum)
  init = np.zeros(k+1,dtype = np.int64)
  coeffs = [(0,1)]
  for i in xrange(1,k+1):
@@ -27,25 +28,23 @@ def s(n,k,b,mod = 10**9+7):
      temp.append((t[0]+num,-t[1]))
    coeffs = temp 
  coeffs = sorted(coeffs)
- ns = set([t[0] for t in coeffs])
- if len(ns) < len(coeffs):
-  print "discrepancy on %d" % k
- return 0
- coeffs.append((n,0))
+ coeffs.append((n,2))
  for curr,next in izip(coeffs[:-1],coeffs[1:]):
-  init[-1] += curr[1]
+  init[-1] = init[-1] + curr[1]
   #shifting factor 
   d = next[0]-curr[0]
   j = 0
   while d:
    if d %2:
-    init = safe_matrix_vector_dot(cache[j],init,mod)
+     init = np.remainder(cache[j].dot(init),mod)
    j += 1
    d /= 2
  return init[-1]
 
 
-#print s(1000,10,5)
-print sum([s(10**k,k,k) for k in xrange(10,16)]) % (10**9+7)
-  
+x=0
+mod = 10**9+7
+for j in xrange(10,16):
+ x = (x+s(10**j,j,j)) % mod
+print x  
   
