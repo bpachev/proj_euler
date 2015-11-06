39fd0ae2e0f43fad6a1e3a8f23c78f99e22dbc12
bpachev <benjaminpachev@gmail.com>
2015-11-05 23:32:36 -0700

    significant work on nanbots.

--- e532.py
+++ e532.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 
 
-def path_len(nbots=3,rinit=.999,niter = 100,delta=2*np.pi/1000,d2=30,tol=1e-6):
+def path_len(nbots=3,rinit=.999,niter = 100,delta=2*np.pi/1000,d2=10,tol=1e-6):
  hinit = (1-rinit**2)**.5
  pos = np.array([rinit,0,hinit])
  theta = 2*np.pi/nbots
@@ -13,7 +13,7 @@ def path_len(nbots=3,rinit=.999,niter = 100,delta=2*np.pi/1000,d2=30,tol=1e-6):
  p2 = np.zeros(3)
  plen = 0.
  i=0
- while abs(1.-pos[2]) > tol:
+ while abs(1.-pos[2]) > tol and i < niter:
   p2[:2] = rotarr.dot(pos[:2])
   p2[2]  = pos[2]
   d = pos.dot(p2)
@@ -23,7 +23,7 @@ def path_len(nbots=3,rinit=.999,niter = 100,delta=2*np.pi/1000,d2=30,tol=1e-6):
   p2 = p2/p2.dot(p2)**.5 #normalize
   pos = cos(ang)*pos + sin(ang)*p2
   i += 1
- print i
+ print i,pos,pos.dot(pos)
  return plen
 
 def path_len2(nbots=3,rinit=.999,niter = 100,delta=.001):
@@ -47,7 +47,7 @@ def path_len2(nbots=3,rinit=.999,niter = 100,delta=.001):
  print pos
  return plen
 
-def path_len3(nbots=3,rinit=.999,tol=1e-6):
+def path_len3(nbots=3,rinit=.999,tol=1e-6,l=1000):
  hinit = (1-rinit**2)**.5
  pos = np.array([rinit,0,hinit])
  theta = 2*np.pi/nbots
@@ -56,15 +56,32 @@ def path_len3(nbots=3,rinit=.999,tol=1e-6):
  plen = 0
  def xfunc(pos,t):
   p2 = np.zeros(3) 
+  if abs(1-pos[2]) < 1e-7:
+   return p2  
   p2[:2] = rotarr.dot(pos[:2])
   p2[2]  = pos[2]
   p2 = p2-np.dot(pos,p2)*pos #orthogonalize
   p2 = p2/p2.dot(p2)**.5 #normalize
   return p2
  
- print odeint(xfunc,pos,np.linspace(0,980),mxstep=20000)
+ print odeint(xfunc,pos,[0,l],mxstep=200000,atol=1e-13,rtol=1e-13)
 
-path_len3(nbots=800)
+
+def path_len4(nbots=3,rinit=.999,tol=1e-6,l=1000):
+ hinit = (1-rinit**2)**.5
+ theta = 2*np.pi/nbots
+ ct = np.cos(theta) 
+ plen = 0
+ def zfunc(z,t):
+  d = ct + (z*z)*(1-ct)
+  return z*((1-d)/(1+d))**.5
+ dom = np.linspace(0,l,int(l*20))
+ sol = odeint(zfunc,hinit,dom,mxstep=200000,atol=1e-10,rtol=1e-10)
+ plt.plot(dom[-100:],sol[-100:])
+ plt.show()
+ print sol[-10:]
+
+path_len4(nbots=833,l=1000.5)
 
 def bisect_search(targ=10):
  nbots = 2
