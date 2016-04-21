c2a72e5911442ea66ad03a4c3e8cd20d9bd97c3c
Benjamin Pachev <benjaminpachev@gmail.com>
2016-04-21 13:54:38 -0600

    Started work on e566

---
+++ gaussian_circle.c
@@ -0,0 +1,48 @@
+#include "proj_euler.h"
+
+// counts the number of gaussian integers with norm <= n
+LL gaussian_circle(LL r)
+{
+  LL res = 1 + 4*r;
+  LL n = r*r;
+  for (LL i = 1; i < r; i++)
+  {
+    res += 4*isqrt(n - i*i);
+//    printf ("%llu\n",  isqrt(n - i*i));
+  }
+  return res;
+}
+
+LL cutoff(LL r, LL i)
+{
+   LL res = isqrt(2*r*i - i*i);
+//   printf ("cutoff(r = %llu, i = %llu) = %llu\n", r, i, res);
+   return res;
+}
+
+LL fast_gaussian_circle(LL r)
+{
+  LL res= 1 + 4*r;
+  LL n = r*r;
+  LL bound1 = isqrt(n/2);
+  LL bound2 = isqrt(r*r - bound1*bound1);
+  LL inc = bound2 * cutoff(r, r-bound2);
+  for (LL i = 1; i < r - bound2; i++)
+  {
+    inc += cutoff(r,i);
+    //inc += (r - i) * (cutoff(r, i) - cutoff(r, i-1));
+  }
+
+  //printf("inc %llu bound %llu, sum %llu\n", inc, bound1, 2*inc - bound1*bound1);
+  return res +  4 * (2*inc - bound1*bound1);
+}
+
+#define CAP 100000000000000LL
+
+int main()
+{
+//  for (LL i =0;i < 1000; i++) printf("%lld ",gaussian_circle(i)-fast_gaussian_circle(i));
+//  LL n = 10000000;
+  printf("real %llu\n", gaussian_circle(n));
+//  printf("fast %llu\n", fast_gaussian_circle(n));
+}
