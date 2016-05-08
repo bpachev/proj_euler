17517c980ba398ae6087e4aa263848d030d2e49a
Benjamin Pachev <benjaminpachev@gmail.com>
2016-05-07 23:11:46 -0600

    Have working e559 code.

--- e559.py
+++ e559.py
@@ -11,17 +11,17 @@ def P(k, r, n):
     divs = np.zeros(dividers+2,np.int64)
     #number of pieces available
     divs[0] = 1
-    divs[1] = ifact[k]
+    divs[1] = pow(int(ifact[k]), r, mod)
     for i in xrange(2, dividers+1):
         for j in xrange(1, i+1):
-            divs[i] -=  (-1) ** (j%2) * pow(ifact[j*k], r, mod) * divs[i-j]
+            divs[i] -=  (-1) ** (j%2) * pow(int(ifact[j*k]), r, mod) * divs[i-j]
             divs[i] %= mod
-#    print divs*fact[n] % mod, rem, q
+#    print divs, rem, q
     for j in xrange(1, len(divs)):
 #        print divs[-j-1], j
-        divs[-1] -= (-1) ** (j%2) * pow(ifact[(j-1)*k+rem], r, mod)  * divs[-j-1]
+        divs[-1] -= (-1) ** (j%2) * pow(int(ifact[(j-1)*k+rem]), r, mod)  * divs[-j-1]
         divs[-1] %= mod
-    return (divs[-1] * pow(fact[n], r, mod)) % mod
+    return (divs[-1] * pow(int(fact[n]), r, mod)) % mod
 
 def Q(n):
     tot = 0
@@ -29,4 +29,4 @@ def Q(n):
         tot += P(k, n, n)
     return tot
 
-print P(1,2,3)
+print P(7,5,30)
