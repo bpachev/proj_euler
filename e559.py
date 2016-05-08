2859fee7223458ef9a83495817dde1f79450ecd5
Benjamin Pachev <benjaminpachev@gmail.com>
2016-05-07 23:28:28 -0600

    Made code faster with numpy and caching.

--- e559.py
+++ e559.py
@@ -2,9 +2,10 @@ import proj_euler as pe
 import numpy as np
 #n = 5*10**4
 mod = 1000000123
-fact, ifact = pe.mod_fact_cache(mod, max_n = 5*10**3+2)
+fact, ifact = pe.mod_fact_cache(mod, max_n = 5*10**4+2)
 
 def P(k, r, n):
+    if k == n: return 1
     q, rem = divmod(n-1,k)
     rem += 1
     dividers = q
@@ -12,11 +13,11 @@ def P(k, r, n):
     #number of pieces available
     divs[0] = 1
     divs[1] = pow(int(ifact[k]), r, mod)
+    pows = np.array([(-1) ** (j%2) * pow(int(ifact[j*k]), r, mod) for j in xrange(dividers+2)])
     for i in xrange(2, dividers+1):
-        for j in xrange(1, i+1):
-            divs[i] -=  (-1) ** (j%2) * pow(int(ifact[j*k]), r, mod) * divs[i-j]
-            divs[i] %= mod
-#    print divs, rem, q
+        temp = np.remainder(pows[1:i+1] * divs[i-1::-1], mod)
+        divs[i] -= np.sum(temp)
+        divs[i] %= mod
     for j in xrange(1, len(divs)):
 #        print divs[-j-1], j
         divs[-1] -= (-1) ** (j%2) * pow(int(ifact[(j-1)*k+rem]), r, mod)  * divs[-j-1]
@@ -27,6 +28,8 @@ def Q(n):
     tot = 0
     for k in xrange(1,n+1):
         tot += P(k, n, n)
+        tot %= mod
     return tot
 
-print P(7,5,30)
+#print P(7,5,30)
+print Q(50000)
