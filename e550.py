4b09a89ae03856281baa4f6f84d417bb6e5408c1
Benjamin Pachev <benjaminpachev@gmail.com>
2016-05-11 22:16:50 -0600

    Worked more on e550, but still have an error somewhere.

--- e550.py
+++ e550.py
@@ -1,6 +1,7 @@
 import numpy as np
 import proj_euler as pe
 from itertools import product
+from CR_NEWTON_LAGRANGE import Lagrange_cr as cr
 
 def num_factor_sieve(n):
   res = np.zeros(n+1, dtype = int)
@@ -47,12 +48,97 @@ def factor_num_to_nimber(n):
 
   return l
 
-def compute_nimber(class):
-    pass
+def count_representatives(c, n, pcounts, primes, excluded=set(), upper = None):
+    """
+    How many numbers with a given type of prime factorization less than n
+    """
+    if n == 0: return 0
+    if len(c) == 1:
+        if not c[0]: return 1
+        n = int(n**(1./c[0]))
+        if upper is not None:
+            n = min(upper, n)
+        return pcounts[n] - sum([1 if el <= n else 0 for el in excluded])
 
-def count_representatives(class, ):
-    pass
 
-#print factor_num_to_nimber(23)
-print pe.prime_equivalences(10**7)[0]
+    tot = 0
+    for p in primes:
+        new_n = n / p**c[0]
+        if not new_n:
+            break
+        if upper is not None and p >= upper:
+            break
+        if p in excluded: continue
+        excluded.add(p)
+        if c[1] == c[0]: tot += count_representatives(c[1:], new_n, pcounts, primes, excluded, p)
+        else: tot += count_representatives(c[1:], new_n, pcounts, primes, excluded)
+        excluded.remove(p)
+
+    return tot
+
+def class_divides(d, n):
+    for el1, el2 in zip(d, n):
+        if el1 > el2:
+            return False
+    return True
+
+def mex(l):
+    l = sorted(list(l))
+    for i, el in enumerate(l):
+        if el > i: return i
+    return len(l)
+
+if __name__ == "__main__":
+    #print factor_num_to_nimber(23)
+#    n = 10
+#    k = 5
+    n = 10**7
+    k = 10**12
+    mod = 987654321
+    m1,m2 = 379721, 3**2*17**2
+    assert m1*m2 == mod
+    primes, mask = pe.primes_and_mask(n)
+    pcounts = np.cumsum(mask.astype(np.int32))
+    classes = pe.prime_equivalences(n)[0]
+    classes.sort(key = lambda c: sum(c))
+    nimber_cache = {}
+#    print pcounts
+    tot = 0
+    for i,c in enumerate(classes):
+
+#        tot += count_representatives(c[:first_zero], n, pcounts, primes)
+        divs = []
+        for e in classes[:i]:
+            if class_divides(e, c):
+                divs.append(nimber_cache[tuple(e)])
+
+        vals = set()
+        for d1, d2 in product(divs, divs):
+            vals.add(d1^d2)
+        nimber_cache[tuple(c)] = mex(vals)
+
+
+    nnimbers = max(nimber_cache.values())
+    nbits = len(bin(nnimbers - 1))-1
+    nstates = 2**nbits
+    print nstates, nnimbers, nbits
+    init = np.zeros(nstates, dtype = np.int64)
+    for c in nimber_cache:
+        first_zero = len(c)
+        for j, el in enumerate(c):
+            if el == 0:
+                first_zero = j
+                break
+        init[nimber_cache[c]] += count_representatives(c[:first_zero], n, pcounts, primes)
+    T = np.zeros((nstates, nstates), dtype = np.int64)
+    for start in xrange(nstates):
+        for transition in xrange(nstates):
+            T[transition^start, start] = init[transition]
+    rems = []
+    for mod in (m1,m2):
+        res = pe.matrix_mod_exp(k-1, T, init, mod)
+        rems.append((np.sum(res)-res[0]) % mod)
+    print cr(rems, (m1,m2))
+#    print tot, n
+
 #solve(30,10**12,987654321)
