bae4a4f7bc428a1f29f01d1ab0d91d9dcfcea521
Benjamin Pachev <benjaminpachev@gmail.com>
2016-05-28 17:17:58 -0600

    Finished e165

--- e163.py
+++ e163.py
@@ -1,13 +1,18 @@
 import numpy as np
+from numpy import pi
 import numpy.linalg as la
+import pylab as pl
+from matplotlib import collections  as mc
+
+
 """
 Project Euler problem 163: Cross-hatched triangles.
 """
 
 class lineSegment:
     def __init__(self, start, finish):
-        self.a = start
-        self.b = finish
+        self.a = np.array(start)
+        self.b = np.array(finish)
 
     def intersects(self, other):
         #l1*a + (1-l1) *b = l2 * oa + (1-l2) * ob
@@ -16,11 +21,15 @@ class lineSegment:
         print lambdas
         return (lambdas[0] > 0 and lambdas[0] < 1) and (lambdas[1] > 0 and lambdas[1] < 1)
 
-L1 = lineSegment(np.array([27, 44]), np.array([12, 32]))
-L2 = lineSegment(np.array([46, 53]), np.array([17, 62]))
-L3 = lineSegment(np.array([46, 70]), np.array([22, 40]))
+#L1 = lineSegment(np.array([27, 44]), np.array([12, 32]))
+#L2 = lineSegment(np.array([46, 53]), np.array([17, 62]))
+#L3 = lineSegment(np.array([46, 70]), np.array([22, 40]))
 
-print L1.intersects(L3)
+#print L1.intersects(L3)
+
+def rot(theta):
+    c,s = np.cos(theta), np.sin(theta)
+    return np.array([[c, s],[-s, c]])
 
 def solve(n):
     """
@@ -30,4 +39,29 @@ def solve(n):
     In the other class (containing orientations 90 deg, 210 deg, and 330 deg) have 2*n - 1 copies each.
     Thus we see that each class of triangles corresponds three types of lines.
     """
-    pass
+
+    theta1 = pi/6
+    h = np.cos(theta1)
+
+    #let the corner be at the bottom left
+    lines = []
+    #first do horizontal lines
+    horiz = []
+    for i in xrange(n):
+        horiz.append(lineSegment(np.array([.5*(i),i*h]), np.array([n-.5*i,i*h])))
+    lines.append(horiz)
+
+    vert = []
+    for i in xrange(1,2*n):
+        vert.append(lineSegment([i*.5, 0],[i*.5, min(i,2*n-i)*h]))
+    lines.append(vert)
+
+    lines = map(lambda s: [tuple(s.a), tuple(s.b)], reduce(lambda a,b: a+b,lines))
+    lc = mc.LineCollection(lines,  linewidths=2)
+    fig, ax = pl.subplots()
+    ax.add_collection(lc)
+    ax.autoscale()
+    ax.margins(0.1)
+    pl.show()
+
+solve(2)
--- e165.py
+++ e165.py
@@ -1,9 +1,8 @@
 import numpy as np
 import numpy.linalg as la
 from itertools import combinations
+from fractions import Fraction as frac
 
-
-tol = 1e-10
 class lineSegment:
     def __init__(self, start, finish):
         self.a = start
@@ -12,47 +11,54 @@ class lineSegment:
     def intersection(self, other):
         #l1*a + (1-l1) *b = l2 * oa + (1-l2) * ob
         try:
+
             A = np.array([self.a-self.b, other.b-other.a]).T
-            lambdas = la.solve(A, other.b - self.b)
-            if (lambdas[0] > 0 and lambdas[0] < 1) and (lambdas[1] > 0 and lambdas[1] < 1):
-                return lambdas[0] * (self.a-self.b) + self.b
-            else:
+            b = other.b - self.b
+            d = int(A[0,0]*A[1,1]-A[1,0]*A[0,1])
+
+            if not d:
                 return None
+
+            l1 = int(b[0]*A[1,1] - b[1]*A[0,1])
+            l2 = int(b[1]*A[0,0] - b[0]*A[1,0])
+            l1 = frac(l1, d)
+            l2 = frac(l2, d)
+
+            if l1 <=0 or l1 >= 1 or l2 <= 0 or l2 >= 1: return None
+
+            return (int(self.a[0]-self.b[0]) * l1 + self.b[0], int(self.a[1]-self.b[1]) * l1 + self.b[1])
+
         except:
             return None
 
-#L1 = lineSegment(np.array([27, 44]), np.array([12, 32]))
-#L2 = lineSegment(np.array([46, 53]), np.array([17, 62]))
-#L3 = lineSegment(np.array([46, 70]), np.array([22, 40]))
+    def __str__(self):
+        return str(self.a) + " " + str(self.b)
+
+L1 = lineSegment(np.array([27, 44]), np.array([12, 32]))
+L2 = lineSegment(np.array([46, 53]), np.array([17, 62]))
+L3 = lineSegment(np.array([46, 70]), np.array([22, 40]))
 
 def dist_intersections(segments):
-    res = []
+    res = set()
     for L1, L2 in combinations(segments, 2):
         p = L1.intersection(L2)
         if p is not None:
-            res.append(tuple(p))
-    res = sorted(res)
-    last = res[0]
-    tot = 1
-    for el in res:
-        if abs(el[0]-last[0]) < tol and abs(el[1] - last[1]) < tol:
-            continue
-        else:
-            last = el
-            tot += 1
-
-    return tot
+            res.add(p)
+    return len(res)
 
 def bbshum_lines(nlines):
     res = []
-    mat = np.zeros(4*nlines)
+    mat = np.zeros(4*nlines, dtype=np.int64)
     s = 290797
     for i in xrange(4*nlines):
         if i and i%4 == 0:
             res.append(lineSegment(mat[i-4:i-2], mat[i-2:i]))
         s = s*s % 50515093
         mat[i] = s % 500
-#    print mat
+    i = 4*nlines
+    res.append(lineSegment(mat[i-4:i-2], mat[i-2:i]))
+
     return res
 
 print dist_intersections(bbshum_lines(5000))
+#print dist_intersections([L1, L2, L3])
