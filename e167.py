2ae8d89bc8e753715a72d526a9fad2cebbdbad4e
Benjamin Pachev <benjaminpachev@gmail.com>
2016-05-31 17:23:51 -0600

    Finished Ulam Sequnces.

--- e167.py
+++ e167.py
@@ -30,25 +30,39 @@ def arr_to_bin(arr):
     return res
 
 
-def find_ulam_period(v):
+def find_ulam_num(v, n):
     a = ulam(2,v,v+3)
     last = a[-1]
     mask = np.array([el in a for el in xrange(last-2*v,last+1,2)])
-    i = 0
     bmask = arr_to_bin(mask)
     tmask = bmask
     p = 2**v
-    diffs = []
+    d = 0
+    diffs = [0]
     while True:
         new = (p * (tmask%2)) ^ (p & tmask)
         tmask /= 2
         tmask += new
-        i += 1
+        d += 2
+        if new:
+            diffs.append(d)
+            d = 0
+
         if tmask == bmask:
-            return 2*i
+            break
 
-find_ulam_period(21)
+    diffs = np.cumsum(np.array(diffs))
+    N = len(diffs)-1
+    D = diffs[-1]
+    diff = n-len(a)
+    if diff < 0:
+        return a[n-1]
+#    print diffs, a[-1], N, D
+    return D * (diff / N) + diffs[diff % N] + a[-1]
 
+print sum ([find_ulam_num(2*i+1, 10**11) for i in xrange(2, 11)])
+#print find_ulam_num(9,37)
+#print ulam(2,9,37)
 #for i in xrange(10,11):
 #    a =  np.array(ulam(2,2*i+1,300), dtype=int)
 #    print a[1:]-a[:-1]
